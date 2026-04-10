0530ff0abbef7dbcdc489997f806febc910be187
Van Phuong <email_cua_ban@gmail.com>
2026-04-10 20:52:10 +0700

    Step 5: Thuc hien Migration tao Database tu code

--- BT Code First/Models/AppDbContext.cs
+++ BT Code First/Models/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace BT_Code_First.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BT_Code_First.Models
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Máy sẽ dựa vào đây để tạo bảng tên là "Books" trong SQL Server
+        public DbSet<Book> Books { get; set; }
     }
 }
--- BT Code First/Models/Book.cs
+++ BT Code First/Models/Book.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BT_Code_First.Models
 {
@@ -9,6 +10,8 @@ namespace BT_Code_First.Models
         [Required] 
         public string Title { get; set; }
         public string Author { get; set; }
+       
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
